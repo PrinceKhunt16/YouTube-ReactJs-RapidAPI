15d9a02814f43e416c133c5491d3772ec79266fe
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-17 15:21:24 +0530

    Add scroll to top

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,16 +1,14 @@
-import { Box, Stack } from "@mui/material";
-import { useEffect, useRef } from "react";
+import { Box } from "@mui/material";
+import { useEffect } from "react";
 import { VideoCard, ChannelCard } from "./"
 
 export default function Videos({ videos }) {
-  const videosList = useRef()
-  
   useEffect(() => {
-    videosList.current.scrollIntoView()
-  }, [])
+    window.scrollTo(0, 0)
+  }, [videos])
 
   return (
-    <div className="videos-list" ref={videosList}>
+    <div className="videos-list">
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} /> }
