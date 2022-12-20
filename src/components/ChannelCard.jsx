b99c441fc63f584c5a2bbe03f074c5a6e8fc3c88
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-20 21:42:55 +0530

    Topbar done with scroll

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -12,21 +12,26 @@ export default function ChannelCard({ channelDetail }) {
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          height: '200px',
+          height: '0px',
+          width: '100%',
           borderRadius: '10px',
           margin: 'auto',
-          background: '#11111110'
+          background: '#11111110',
+          position: 'relative',
+          paddingTop: '56.25%'
         }}
       >
-        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-          <div className='channel-card'>
-            <CardMedia
-              image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
-              alt={channelDetail?.snippet?.title}
-              sx={{ borderRadius: '50%', height: '150px', width: '150px' }}
-            />
-          </div>
-        </Link>
+        <div className='channel-card-body'>
+          <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+            <div className='channel-card'>
+              <CardMedia
+                image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                alt={channelDetail?.snippet?.title}
+                sx={{ borderRadius: '50%', height: '150px', width: '150px' }}
+              />
+            </div>
+          </Link>
+        </div>
       </Box>
       <Typography sx={{ fontSize: '16px', fontWeight: 400, color: 'black', marginTop: '10px' }}>
         {channelDetail?.snippet?.title}
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -24,6 +24,7 @@ export default function Topbar({ selectedCategory, setSelectedCategory }) {
           </span>
         </button>
       ))}
+      <div className="white-box"></div>
     </Stack>
   )
 }
